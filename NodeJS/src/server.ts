c2d14abcae5f811c32e3e45bf89a2dfb09de3e1f
Marcelo Figueira <60945481+marcelosousafigueira@users.noreply.github.com>
2022-06-25 20:52:40 -0300

    Aula Node

--- NodeJS/src/server.ts
+++ NodeJS/src/server.ts
@@ -1,11 +1,8 @@
-import express, { Request, Response } from "express";
-import { createCouse } from "./routes";
+import express from "express";
+import { categoriesRoutes } from "./routes/categories.routes";
 
 const app = express();
-app.get("/", (request: Request, response: Response) => {
-  return response.json({ message: "Hello" });
-});
-
-app.get("/teste", createCouse);
+app.use(express.json());
+app.use("/categoria", categoriesRoutes);
 
 app.listen(3333);
